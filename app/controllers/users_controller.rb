7053514ae3f10650fbc9b8e30b3464761d9313c6
Christopheraarguello <christopheraarguello@gmail.com>
2017-09-14 19:38:40 -0400

    created users model, populated with username,password,email. Created index page.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:users].inspect
+    @users = User.new(params.require(:users).permit(:username, :password, :email))
+    @users.save
+    redirect_to @users
+  end
+
+  def show
+    @users = User.find(params[:id])
+  end
+
+end
