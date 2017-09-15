f94c788b545d5fb6620d700af8d6e5c85c35b753
Christopheraarguello <christopheraarguello@gmail.com>
2017-09-15 14:05:43 -0400

    pushing some additions to the tournament model and tournament creation

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,10 +1,21 @@
 class TournamentsController < ApplicationController
   def index
     @tournaments = Tournament.all
-
   end
 
   def show
     @tournament = Tournament.find(params[:id])
-  end 
+  end
+
+  def new
+    @tournament = Tournament.new
+  end
+
+  def create
+  end
+
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tournaments, :join_table => :users_tournaments
+
+  validates(:username,
+    presence: true,
+    length: {minimum:4, maximum:15})
+  validates(:email,
+    presence: true,
+    length: {minimum:4, maximum:50})
+  validates(:password,
+    presence: true,
+    length: {minimum:4, maximum:50})
+
+
+
 end
