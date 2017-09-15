9a056ecf6ca21c5d04481c2c220b135430a3bbdf
Alfonso Canales <canales101@yahoo.com>
2017-09-14 20:56:24 -0400

    added a tournament index view and index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
+  get '/tournaments' => 'tournaments#index'
+
   # resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
